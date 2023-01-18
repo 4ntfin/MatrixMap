c08f2f9987f2fe8cb44a6be46096ec373f9da2a6
4ntfin <110556015+4ntfin@users.noreply.github.com>
2023-01-17 19:08:17 -0600

    Add project files.

---
+++ MatrixMap/MatrixMap.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace MatrixMap
+{
+    partial class MatrixMap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MatrixMap));
+            this.tlpMap = new System.Windows.Forms.TableLayoutPanel();
+            this.ilsTiles = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // tlpMap
+            // 
+            this.tlpMap.AutoSize = true;
+            this.tlpMap.BackColor = System.Drawing.Color.OliveDrab;
+            this.tlpMap.ColumnCount = 10;
+            this.tlpMap.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMap.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMap.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMap.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMap.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMap.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMap.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMap.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMap.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMap.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMap.Location = new System.Drawing.Point(0, 0);
+            this.tlpMap.Margin = new System.Windows.Forms.Padding(0);
+            this.tlpMap.Name = "tlpMap";
+            this.tlpMap.RowCount = 10;
+            this.tlpMap.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMap.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMap.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMap.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMap.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMap.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMap.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMap.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMap.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMap.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMap.Size = new System.Drawing.Size(276, 174);
+            this.tlpMap.TabIndex = 0;
+            // 
+            // ilsTiles
+            // 
+            this.ilsTiles.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ilsTiles.ImageStream")));
+            this.ilsTiles.TransparentColor = System.Drawing.Color.Transparent;
+            this.ilsTiles.Images.SetKeyName(0, "tile_0002.png");
+            this.ilsTiles.Images.SetKeyName(1, "tile_0126.png");
+            this.ilsTiles.Images.SetKeyName(2, "tile_0043.png");
+            this.ilsTiles.Images.SetKeyName(3, "tile_0049.png");
+            this.ilsTiles.Images.SetKeyName(4, "tile_0052.png");
+            this.ilsTiles.Images.SetKeyName(5, "tile_0055.png");
+            this.ilsTiles.Images.SetKeyName(6, "tile_0084.png");
+            // 
+            // MatrixMap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tlpMap);
+            this.MaximizeBox = false;
+            this.Name = "MatrixMap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Matrix Map";
+            this.Load += new System.EventHandler(this.MatrixMap_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpMap;
+        private System.Windows.Forms.ImageList ilsTiles;
+    }
+}
+
---
+++ MatrixMap/MatrixMap.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatrixMap
+{
+    public partial class MatrixMap : Form
+    {
+
+        const int NUM_ROWS = 10, NUM_COLS = 10, CELL_SIZE = 32;
+
+        Random rnd = new Random();
+        Label[,] labelArr = new Label[NUM_ROWS , CELL_SIZE];
+        int[] tileInts = new int[] {0, 1, 2, 3, 4, 5};
+
+        private void MatrixMap_Load(object sender, EventArgs e)
+        {
+            this.Width =  CELL_SIZE * NUM_ROWS;
+            this.Height = CELL_SIZE * NUM_ROWS;
+            tlpMap.Width = CELL_SIZE * NUM_ROWS;
+            tlpMap.Height = CELL_SIZE * NUM_ROWS;
+            Console.WriteLine(this.Width);
+            Console.WriteLine(this.Height);
+
+            loadArrays();
+
+
+        }
+
+        private void loadArrays()
+        {
+            for (int i = 0; i < NUM_ROWS; i++)
+            {
+                for (int j = 0; j < NUM_COLS; j++)
+                {
+                    int num =rnd.Next(6);
+                    int t = rnd.Next(tileInts.Length);
+
+                    Label l = new Label();
+                   // l.Text = col.ToString();
+                    l.Width = CELL_SIZE;
+                    l.Height = CELL_SIZE;
+                    
+                    switch(t)
+                    {
+                        case 1:
+                            l.Image = ilsTiles.Images[0];
+                            break;
+                        case 2:
+                            l.Image = ilsTiles.Images[1];
+                            break;
+                        case 3:
+                            l.Image = ilsTiles.Images[2];
+                            break;
+                        case 4:
+                            l.Image = ilsTiles.Images[3];
+                            break;
+                        case 5:
+                            l.Image = ilsTiles.Images[4];
+                            break;
+                        case 0:
+                            l.Image = ilsTiles.Images[5];
+                            break;
+                    }
+
+                    l.Margin = new Padding(0);
+                    l.TextAlign = ContentAlignment.MiddleCenter;
+                   
+                    
+                    tlpMap.Controls.Add(l);
+                    //l.BringToFront();
+                }
+            }
+        }
+
+
+
+
+        public MatrixMap()
+        {
+            InitializeComponent();
+        }
+    }
+}
